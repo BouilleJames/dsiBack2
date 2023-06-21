1bc0aa0d57cc6941d9f48609c5bbeb9d8b9dae72
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-06-21 16:24:57 +0200

    add connexion and hash password

--- brief7definitif2106.sql
+++ brief7definitif2106.sql
@@ -1,33 +1,35 @@
 CREATE DATABASE IF NOT EXISTS dsimed;
+
 USE dsimed;
-CREATE TABLE IF NOT EXISTS `medias` (
-	id_medias INT AUTO_INCREMENT NOT NULL,
-	reference_media VARCHAR(25) NOT NULL,
-	url VARCHAR(255),
-	PRIMARY KEY (`id_medias`)
-	);
-     
-CREATE TABLE IF NOT EXISTS `users` (
-	id_users INT AUTO_INCREMENT NOT NULL,
-    nom varchar(50) NOT NULL,
-    prenom varchar(50) NOT NULL,
-    mot_de_passe varchar(255) NOT NULL,
-    mail varchar(255) UNIQUE NOT NULL,
-    roles  BOOLEAN NOT NULL,
-    PRIMARY KEY (`id_users`)
-	);
-CREATE TABLE IF NOT EXISTS `articles`  (
-	id_articles INT AUTO_INCREMENT NOT NULL,
-    titre varchar(25),
-    content text,
-    date_publication timestamp not null,
-    id_users INT not null,
-    id_medias INT not null,
-    CONSTRAINT fk_id_users FOREIGN KEY (id_users) REFERENCES users(id_users),
-    CONSTRAINT fk_id_medias FOREIGN KEY (id_medias) REFERENCES medias(id_medias),
-    PRIMARY KEY (`id_articles`)
-	);    
 
+CREATE TABLE
+    IF NOT EXISTS `medias` (
+        id_medias INT AUTO_INCREMENT NOT NULL,
+        reference_media VARCHAR(25) NOT NULL,
+        url VARCHAR(255),
+        PRIMARY KEY (`id_medias`)
+    );
 
+CREATE TABLE
+    IF NOT EXISTS `users` (
+        id_users INT AUTO_INCREMENT NOT NULL,
+        nom varchar(50) NOT NULL,
+        prenom varchar(50) NOT NULL,
+        mot_de_passe varchar(255) NOT NULL,
+        mail varchar(255) UNIQUE NOT NULL,
+        roles BOOLEAN NOT NULL,
+        PRIMARY KEY (`id_users`)
+    );
 
-
+CREATE TABLE
+    IF NOT EXISTS `articles` (
+        id_articles INT AUTO_INCREMENT NOT NULL,
+        titre varchar(25),
+        content text,
+        date_publication timestamp not null,
+        id_users INT not null,
+        id_medias INT not null,
+        CONSTRAINT fk_id_users FOREIGN KEY (id_users) REFERENCES users(id_users),
+        CONSTRAINT fk_id_medias FOREIGN KEY (id_medias) REFERENCES medias(id_medias),
+        PRIMARY KEY (`id_articles`)
+    );
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+﻿require("rootpath")();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
 const app = express();
+const errorHandler = require("_middleware/error-handler");
 const port = process.env.PORT || 3004;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// api routes
+app.use("/users", require("./users/user.controller"));
+
+// global error handler
+app.use(errorHandler);
 
 const db = mysql.createConnection({
   host: "localhost",
